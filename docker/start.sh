d6d853ce379519aea2cf43bfc7f06d4acdd30db6
Jordi Sala <jordism91@gmail.com>
2016-12-11 23:38:46 +0100

    fixing

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 
 docker-compose build
 
-if command -v dockedsadsar-sync-stack >/dev/null 2>&1; then
+if command -v docker-sync-stack >/dev/null 2>&1; then
   docker-sync-stack start
 else
   docker-compose -f docker-compose.yml -f docker-compose-linux.yml up
