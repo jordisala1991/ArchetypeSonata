a069dec4504f9631d2a3be34c6afdc6aa058b2ef
Jordi Sala <jordism91@gmail.com>
2017-02-14 21:20:49 +0100

    updated docker config, updated composer vendors, fixed fos_user config

--- docker/configure.sh
+++ docker/configure.sh
@@ -3,5 +3,5 @@
 cp files/parameters.yml ../app/config/
 
 docker exec sonata composer install --optimize-autoloader
-docker exec sonata php app/console doctrine:migrations:migrate --no-interaction
+docker exec sonata php bin/console doctrine:migrations:migrate --no-interaction
 docker exec sonata chown -R www-data:www-data /var/www/html
