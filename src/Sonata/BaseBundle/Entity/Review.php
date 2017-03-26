4f5e2129eed1356b8bc64dcd97f11cc8a59a8ffc
Jordi Sala <jordism91@gmail.com>
2017-03-26 22:52:03 +0200

    Add demo entities

---
+++ src/Sonata/BaseBundle/Entity/Review.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Sonata\BaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ */
+class Review
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @Assert\NotNull
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @Assert\NotNull
+     * @ORM\Column(type="text")
+     */
+    protected $opinion;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $rating;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Book", inversedBy="reviews")
+     * @ORM\JoinColumn(referencedColumnName="id")
+     */
+    protected $book;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Review
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set opinion
+     *
+     * @param string $opinion
+     *
+     * @return Review
+     */
+    public function setOpinion($opinion)
+    {
+        $this->opinion = $opinion;
+
+        return $this;
+    }
+
+    /**
+     * Get opinion
+     *
+     * @return string
+     */
+    public function getOpinion()
+    {
+        return $this->opinion;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     *
+     * @return Review
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * Set book
+     *
+     * @param Book $book
+     *
+     * @return Review
+     */
+    public function setBook(Book $book = null)
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+
+    /**
+     * Get book
+     *
+     * @return Book
+     */
+    public function getBook()
+    {
+        return $this->book;
+    }
+}
