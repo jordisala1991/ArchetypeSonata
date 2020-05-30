0783f8de1387b880d420f1e600a9d5943bd798d6
Jordi Sala Morales <jordism91@gmail.com>
2020-05-30 13:29:55 +0200

    Fix cs

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity;
 
-use App\Entity\Media;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
